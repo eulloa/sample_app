8570c148e242ba82a36610ddf49941a70a4f9d3b
Efren Ulloa <eulloa345@gmail.com>
2017-04-09 20:41:20 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  #root
+  root 'static_pages#home'
   
+  #static_pages
   get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  root 'application#hello'
-  
+  get 'static_pages/help'  
+  get 'static_pages/about'
+  get 'static_pages/contact'
   
 end
